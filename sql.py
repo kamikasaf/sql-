d037e05ab0a823bbda268c187f3949402fffe9c1
Erlashka <slayer6662mail.ru@gmail.com>
2022-05-11 14:08:48 +0600

    Commands POSTGRESQL

--- sql.py
+++ sql.py
@@ -24,29 +24,30 @@ SQL запросы
 """ 
 """команды SQL"""
 
-
+"""
 # sudo -u postgres psql(psql postgres) - войти в sql
 # \l открыть сущ базы данных
-# create database name; создать базу данных
 # \c name законнектиться к базе данных
 # \dt \d увидеть все таблицы
-# drop database name; удалить таблицу
 # \du покажет всех userov
+
+# create database name; создать базу данных
 # create user name_user with password '12345'; - создать нового не главного юзера с паролем
+# create database  name_database with onwer name_user - создать базуданных с другим овнером
+# create table table_name(создавать поля имя поля, тип поля, ограничения)    создать таблицу
 
 # alter role nama_user with superuser дать все привелегии другому юзеру 
-# create database  name_database with onwer name_user - создать базуданных с другим овнером
 
 # grant all privileges on database name_databse to name_user - дать все привелегии на базу данных
 
+# drop database name; удалить таблицу
 # drop role name_user -удалить юзера
-# create table table_name(создавать поля имя поля, тип поля, ограничения)    создать таблицу
+
 # \d name_table подробдное инфо о таблице
 # \dt name_table инфо о таблице
-
-
 # \i /home/erlan/Downloads/students.sql найти и загрузить в sql
-# insert into name_table(перечесление имен полей кроме автодекредетируемых) values(передaем название полей)
+"""
+
 """Типы данных"""
 # 1. numeric types -числовой тип данных
 # 2. character types - как стр
@@ -81,6 +82,7 @@ Select
 # select * from offset 5 limit 5        offset с какого начать не включительно
 # select * from table_name in название поля (перечисление);        поиск
 # select field_name || field_name2 from students; --> || конкатинация строк
+
         AS
 # select field_name || field_name2  as another field_name from students; --> || конкатинация строк
         Order by --> сортирует по какому то полю, по алфовитонму порядку или возрастанию
@@ -89,9 +91,11 @@ Select
 # select field_name from table_name order by field_name desc; убывание
 nulls last пустые поля в конце
 nulls first пустые поля вначале
+
         Distinct избавит от повторяющихся элементов
 # select distinct field_name from table_name;  
 # select distinct on(field_name) field_name, field_name2 from table_name order by field_name, field_name2; 
+
         Where - условие как if
 # select * from table_name where название поля = '';            поиск
 # select * from table_name where название поля = ''  and название поля = '' ;       поиск
@@ -105,20 +109,53 @@ nulls first пустые поля вначале
 # in ('some', 'some', 'some')
 # beetween - между, между возрастом 20 и 30 и т.д
 # select * from table_name where field_name between 20 and 30; 
+
         Like - чтобы в слове было определенное что то типа буквы вначале, в конце, в середине, заканчивалось на что то и т.д
         Ilike наоборот
 # select * from table_name where field_name like 'A%'; где % значит что есть неограниченное продолжение
 # like 'a_' значит что после а есть только 1 символ. _ означает один символ __ 2 и т.д
+
         Is 
 # select * from table_name where last_name is null; 
-        Limit
-        Fetch
-        Offset
-# select * from table_name limit int
-        Group by, Having
-        aggregation functions: SUM(), AVG(), COUNT(), MIN(), MAX()
-Insert
-Update 
-Delete
-Alter Table
+
+        Limit ставит лимит сколько строк вытащить
+        Fetch тот же лимит с другим синтаксисом
+        Offset ставит начало 
+# select * from table_name limit int;
+# select * from table_name fetch first(10) row only;
+# select * from table_name offset 4 fetch first(10) row only;
+# select * from table_name limit 10 offset 5; вытащит 10 строк и начнет с 6;
+
+        Group by - используется с агригационными функциями вытащит столбец с условием из агригационных функций
+        Having - как where только для group by 
+# aggregation functions: SUM(), AVG(), COUNT(), MIN(), MAX()
+# select sum(field_name) from table_name;  --> вытащит сумму какого то поля 
+# select count(field_name) from table_name; --> количество
+# select avg(field_name) from table_name;   --> среднее значение 
+# select sum(field_name) from table_name group by field_name;
+# select sum(field_name) from table_name gorup by field_name having sum(field_name) > 6
+
+        Insert -- додавление в таблицу 
+# insert into name_table(перечесление имен полей кроме автодекредетируемых) values(передaем название полей)
+ 
+        Update -- изменение записи
+# Update table_name Set field_name = value1, field_name2 = value2 where condition; - изменение значений в таблице где condition это условие, (id = 10) где нужно показать какую строку изменить по айди имени или ещ чему нибудь
+
+        Delete --> удаление записи из таблицы
+# Delete from table_name where codition 
+
+        Alter Table --> работа с самой таблицей и ее столбцами
+# alter table table_name add/rename/drop/alter column ...
+
+        alter column --> изменение характеристики столбца (тип, null not null и т.д)
+# alter table alter column field_name (type, set)  
+        rename
+# alter table table_name rename column field_name to new_field_name
+        
+
+        add constrain --> добавление ограничений
+# alter table table_name add constrain name_constrain (field_name);
+        drop --> удаление
+# alter table table_name drop field_name;
+
 """
